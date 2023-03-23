066c123ee51a88edcd5c134f2ba165b067e1fa75
jmvsevilleja <jmvsevilleja@gmail.com>
2023-03-24 06:48:47 +0800

    added robot module in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RobotModule } from './robot/robot.module';
+import { Robot } from './robot/robot.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: '127.0.0.1',
+      port: 27017,
+      database: 'robots',
+      entities: [Robot],
+      synchronize: true,
+      useUnifiedTopology: true,
+    }),
+    RobotModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
